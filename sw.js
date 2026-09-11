d146b5f14e551b3f392ef765d47670489bc90adb
Moanjinhuan <2328214118@qq.com>
2026-09-11 21:21:11 +0800

    Site updated: 2026-09-11 21:21:09

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1789131153924";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1789132868499";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
